ca2de2272f30f8e318f821ec7cd9cf096b1b08af
ericksonkatie <156682607+ericksonkatie@users.noreply.github.com>
2024-01-31 16:20:49 +0000

    solved

--- lectures/w03/bin_packing_charlotte.py
+++ lectures/w03/bin_packing_charlotte.py
@@ -1,23 +1,25 @@
-def first_fit(item_list, bin_size):
 
+
+def first_fit(item_list, bin_size):
     # Initialise a list of bins
     bin_list = [0]
 
     for item in item_list:
-        # Take our ith item
-        if item <= bin_size - bin_list[0]:
-            bin_list[0] += item
-        
-        else:
+        # Try to fit the item in the existing bins
+        fit_found = False
+        for i in range(len(bin_list)):
+            if item <= bin_size - bin_list[i]:
+                bin_list[i] += item
+                fit_found = True
+                break
+
+        # If the item didn't fit in any existing bin, create a new bin
+        if not fit_found:
             bin_list.append(item)
-        
-    
+
     return bin_list
 
-
-
 # Data we have:
-
 # size of each item
 item_list = [2, 1, 3, 2, 1, 2, 3, 1]
 
@@ -30,3 +32,5 @@ bin_size = 4
 # can be obtained from len(bin_list).
 
 print(first_fit(item_list, bin_size) == [4, 4, 4, 3])
+
+
